42320123a38973120c6bb7c2ef71d30e069a882b
tarminik <tarminik@gmail.com>
2024-12-29 14:26:30 +0300

    separate positions implemented

--- CDP.sol
+++ CDP.sol
@@ -9,14 +9,19 @@ contract CDP {
     uint256 public stableCoinPrice = 1; // 1 stablecoin = 1 RUB
     uint256 public ethToRubPrice = 100; // Example ETH price in RUB
 
-    mapping(address => uint256) public collateralETH;
-    mapping(address => uint256) public debtStableCoin;
+    struct Position {
+        uint256 collateralETH;
+        uint256 debtStableCoin;
+    }
+
+    mapping(address => mapping(uint256 => Position)) public userPositions; // user -> position ID -> Position
+    mapping(address => uint256) public positionCount; // Tracks the number of positions for each user
 
     StableCoin public stableCoin;
 
-    event CDPCreated(address indexed user, uint256 collateral, uint256 debt);
-    event CDPRedeemed(address indexed user, uint256 collateral, uint256 repaidDebt);
-    event Liquidated(address indexed user, uint256 collateralUsed);
+    event CDPCreated(address indexed user, uint256 positionId, uint256 collateral, uint256 debt);
+    event CDPRedeemed(address indexed user, uint256 positionId, uint256 collateral, uint256 repaidDebt);
+    event Liquidated(address indexed user, uint256 positionId, uint256 collateralUsed);
 
     constructor(address _stableCoinAddress) {
         owner = msg.sender;
@@ -34,40 +39,55 @@ contract CDP {
 
     function createCDP() external payable {
         require(msg.value > 0, "Must deposit ETH");
+
         uint256 maxMintable = (msg.value * ethToRubPrice * 100) / (collateralizationRatio * stableCoinPrice);
         require(maxMintable > 0, "Insufficient collateral");
 
-        collateralETH[msg.sender] += msg.value;
-        debtStableCoin[msg.sender] += maxMintable;
+        uint256 positionId = positionCount[msg.sender];
+        userPositions[msg.sender][positionId] = Position({
+            collateralETH: msg.value,
+            debtStableCoin: maxMintable
+        });
+
+        positionCount[msg.sender]++;
 
         stableCoin.mint(msg.sender, maxMintable);
 
-        emit CDPCreated(msg.sender, msg.value, maxMintable);
+        emit CDPCreated(msg.sender, positionId, msg.value, maxMintable);
     }
 
-    function redeemCDP(uint256 stableCoinAmount) external {
-        require(debtStableCoin[msg.sender] >= stableCoinAmount, "Not enough debt to repay");
+    function redeemCDP(uint256 positionId, uint256 stableCoinAmount) external {
+        Position storage position = userPositions[msg.sender][positionId];
+        require(position.debtStableCoin >= stableCoinAmount, "Not enough debt to repay");
 
         uint256 collateralToReturn = (stableCoinAmount * collateralizationRatio * stableCoinPrice) / (100 * ethToRubPrice);
-        require(collateralETH[msg.sender] >= collateralToReturn, "Not enough collateral");
+        require(position.collateralETH >= collateralToReturn, "Not enough collateral");
 
-        debtStableCoin[msg.sender] -= stableCoinAmount;
-        collateralETH[msg.sender] -= collateralToReturn;
+        position.debtStableCoin -= stableCoinAmount;
+        position.collateralETH -= collateralToReturn;
+
+        if (position.debtStableCoin == 0 && position.collateralETH == 0) {
+            delete userPositions[msg.sender][positionId]; // Clean up if position is fully redeemed
+        }
 
         stableCoin.burn(msg.sender, stableCoinAmount);
         payable(msg.sender).transfer(collateralToReturn);
 
-        emit CDPRedeemed(msg.sender, collateralToReturn, stableCoinAmount);
+        emit CDPRedeemed(msg.sender, positionId, collateralToReturn, stableCoinAmount);
     }
 
-    function liquidate(address user) external {
-        uint256 requiredCollateral = (debtStableCoin[user] * collateralizationRatio * stableCoinPrice) / (100 * ethToRubPrice);
-        require(collateralETH[user] < requiredCollateral, "CDP is not undercollateralized");
+    function liquidate(address user, uint256 positionId) external {
+        Position storage position = userPositions[user][positionId];
 
-        uint256 collateralUsed = collateralETH[user];
-        collateralETH[user] = 0;
-        debtStableCoin[user] = 0;
+        uint256 requiredCollateral = (position.debtStableCoin * collateralizationRatio * stableCoinPrice) / (100 * ethToRubPrice);
+        require(position.collateralETH < requiredCollateral, "Position is not undercollateralized");
 
-        emit Liquidated(user, collateralUsed);
+        uint256 collateralUsed = position.collateralETH;
+        position.collateralETH = 0;
+        position.debtStableCoin = 0;
+
+        delete userPositions[user][positionId]; // Remove the position after liquidation
+
+        emit Liquidated(user, positionId, collateralUsed);
     }
 }
